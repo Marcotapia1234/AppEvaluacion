7d202017a2394fa2d522b7612d661f371e4e215b
Marco <empleomarcotapia@gmail.com>
2024-05-07 07:10:20 -0700

    Initial Commit

---
+++ AppEvaluacion/AppEvaluacionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppEvaluacionApp.swift
+//  AppEvaluacion
+//
+//  Created by Marco on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AppEvaluacionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
