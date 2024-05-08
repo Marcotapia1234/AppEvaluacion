53ed72250a71fd8926a3783bfe2455a3b2784041
Marco <empleomarcotapia@gmail.com>
2024-05-08 10:17:32 -0700

    Implementación de EvaluaView

--- AppEvaluacion/EvaluaView.swift
+++ AppEvaluacion/EvaluaView.swift
@@ -8,15 +8,67 @@
 import SwiftUI
 
 struct EvaluaView: View {
+   // @State private var rectangleColorChange = false
+    @State private var text: String = ""
+    @State private var text1: String = ""
     var body: some View {
+        VStack {
+                 
+            ExtractedView()
+            HStack{
+                CaritaView (texto:"ALTO", icon: "hand.thumbsup")
+                CaritaView (texto:"MEDIO", icon: "hand.point.right")
+                CaritaView (texto:"BAJO", icon: "hand.thumbsdown")
+            }
+            
+            Text("Qué te ha parecido el taller")
+                .fontWeight(.black)
+            
+            TextField("Ingrese texto aquí", text: $text)
+                            .padding()
+                            .textFieldStyle(RoundedBorderTextFieldStyle())
+
+             
+            Text("Has aprendido algo nuevo")
+                .fontWeight(.black)
+            
+            TextField("Ingrese texto aquí", text: $text)
+                            .padding()
+                            .textFieldStyle(RoundedBorderTextFieldStyle())                    }
+                    .padding()
         
-        
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+    
 }
 
 struct EvaluaView_Previews: PreviewProvider {
     static var previews: some View {
         EvaluaView()
+    }
+}
+
+struct ExtractedView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 2){
+            Text("Valora el taller")
+                .font(.system(.title, design: .rounded))
+                .fontWeight(.black)
+        }
+    }
+}
+struct CaritaView: View{
+    var texto:String
+    var icon:String
+    var body: some View{
+        VStack{
+            
+                Text(texto)
+                
+                Image(systemName: icon)
+                    .font(.system(size: 70))
+                
+            
+            
+        }
     }
 }
